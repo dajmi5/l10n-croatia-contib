aef6646e6622b3efd6c451b7c79cb52e85c03c3f
Davor Bojkić - Bole <bole@dajmi5.com>
2023-01-14 17:07:28 +0100

    [add] l10n_hr_account_reference

---
+++ l10n_hr_account_reference/__manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name": """Account reference: Poziv na broj""",
+    "summary": """Add reference type: Poziv na broj""",
+    "category": "Accounting",
+    "images": [],
+    "version": "16.0.1.0.0",
+    "application": False,
+    "author": "Ecodica ltd, Daj Mi 5",
+    "licence": "AGPL-3",
+
+    "depends": [
+        'l10n_hr_account_base'
+    ],
+    "data": [
+        "views/account_journal_view.xml",
+        "data/pnbr_default_property.xml",
+        "views/report_invoice.xml"
+    ],
+    "demo": [],
+
+    #"post_load": None,
+    #"pre_init_hook": None,
+    #"post_init_hook": None,
+
+    "auto_install": False,
+    "installable": True,
+}
---
+++ l10n_hr_account_reference/models/__init__.py
@@ -0,0 +1,5 @@
+
+from . import account_journal
+from . import poziv_na_broj
+from . import account_move
+
